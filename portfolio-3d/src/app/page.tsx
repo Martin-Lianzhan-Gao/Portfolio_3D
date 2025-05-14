0300662ea4cc4d799f9456c9a231f4fc3e091997
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-05-14 17:09:40 +1000

    chore(project): setup project structure

---
+++ portfolio-3d/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+            My first 3D Portfolio
+            
+        </div>
+    )
+}
